74bb64b0996437d76b940bf08fb63ff04f1e4cc4
Anna Bertmark <annamy.bertmark@gmail.com>
2018-01-27 19:13:03 +0000

    Adding dialogs

---
+++ Assets/Retroverse/Scripts/Dialogs/NumPlayersDialog.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumPlayersDialog : MonoBehaviour {
+
+	[SerializeField]
+	Players players;
+
+	[SerializeField]
+	DialogTransistion transition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SetNumPlayers(int i){
+		players.Setup(i);
+
+		transition.Show();
+	}
+}
