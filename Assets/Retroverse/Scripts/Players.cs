489d883034a1d21ca2f76893cc496ca8b4cc59c0
Anna Bertmark <annamy.bertmark@gmail.com>
2018-01-27 00:32:27 +0000

    Meh, nothing much

--- Assets/Retroverse/Scripts/Players.cs
+++ Assets/Retroverse/Scripts/Players.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "Players/PlayerCollection")]
 public class Players : ScriptableObject
 {
     List<Player> players;
--- Assets/Retroverse/Scripts/Questions.cs
+++ Assets/Retroverse/Scripts/Questions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "Questions/QuestionCollection")]
 public class Questions : ScriptableObject
 {
     List<Question> question;
