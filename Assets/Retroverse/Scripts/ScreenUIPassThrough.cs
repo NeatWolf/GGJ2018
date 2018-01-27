ddbe85d79d9362570065d017be46e6d859531a3c
Anna Bertmark <annamy.bertmark@gmail.com>
2018-01-27 17:45:08 +0000

    Added ceefax color palette as color32 values

---
+++ Assets/Retroverse/Scripts/ScreenUIPassThrough.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ScreenUIPassThrough : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler {
+
+	[SerializeField]
+	GraphicRaycaster raycaster;
+
+	[SerializeField]
+
+	Camera mainCamera;
+
+
+	[SerializeField]
+	LayerMask layerMask;
+
+	private List<RaycastResult> results = new List<RaycastResult>();
+	public void OnPointerClick(PointerEventData eventData)
+    {
+		RaycastHit hit;
+		if( Physics.Raycast( mainCamera.ViewportPointToRay(eventData.position), out hit, 1000f, layerMask ) ){
+			eventData.position = hit.textureCoord;
+			results.Clear();
+			raycaster.Raycast(eventData, results );
+
+		}
+		
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+		results.Clear();
+        raycaster.Raycast(eventData, results );
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+		results.Clear();
+        raycaster.Raycast(eventData, results );
+    }
+}
--- Assets/Retroverse/Scripts/Settings.cs
+++ Assets/Retroverse/Scripts/Settings.cs
@@ -1,16 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Settings : MonoBehaviour {
+[CreateAssetMenu]
+public class Settings : ScriptableObject 
+{
+	public Color32[] palette;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
