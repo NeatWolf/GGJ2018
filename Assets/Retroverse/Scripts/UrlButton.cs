0cde94f07fc0a12c64664e0cc42c0e8719d620d2
Anna Bertmark <annamy.bertmark@gmail.com>
2018-01-28 14:51:38 +0000

    Removed Evans Main. Added url button class

---
+++ Assets/Retroverse/Scripts/UrlButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UrlButton : MonoBehaviour {
+
+	[SerializeField]
+	Button button;
+
+	[SerializeField]
+	TextAsset readmeUrl;
+
+	// Use this for initialization
+	void Awake () {
+		button.onClick.AddListener( ()=>{
+			Application.OpenURL( readmeUrl.text );
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
