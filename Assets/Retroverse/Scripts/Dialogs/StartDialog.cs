b46c360b53a600d02fffe74ac4348011bdd8c00c
Evan Griffiths <evangriffiths@hotmail.co.uk>
2018-01-28 14:29:59 +0000

    Made StartDialog, added credits to game (can now be accessed)

---
+++ Assets/Retroverse/Scripts/Dialogs/StartDialog.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GGJ2018 {
+	public class StartDialog : Dialog {
+
+		[SerializeField]
+		private Button startButton, creditsButton;
+
+		[SerializeField]
+		private Dialog numPlayersDialog, creditsDialog;
+		
+		[SerializeField]
+		private DialogTransistion transistion;
+
+		public void LoadCredits () {
+			Hide();
+			transistion.Show(creditsDialog);
+		}
+
+		public void LoadNumPlayers () {
+			Hide();
+			transistion.Show(numPlayersDialog);
+		}
+
+		public override void Init(){
+
+			startButton.onClick.AddListener( ()=>{
+				LoadNumPlayers();
+			});
+
+			creditsButton.onClick.AddListener( ()=> {
+				LoadCredits();
+			});
+		}
+
+		// Use this for initialization
+		void Start () {
+			
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
+		}
+	}
+}
