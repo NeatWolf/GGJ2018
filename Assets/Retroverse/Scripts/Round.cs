9442f951d0416708d02229bdcd1d4c23091d10e9
Anna Bertmark <annamy.bertmark@gmail.com>
2018-01-28 13:41:20 +0000

    Round resets now on load

--- Assets/Retroverse/Scripts/Round.cs
+++ Assets/Retroverse/Scripts/Round.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu]
-public class Round : ScriptableObject {
+public class Round : ScriptableObject, ISerializationCallbackReceiver {
 	public int roundNum = 1;
 	public List<string> guesses;
 	public ClipRecording playerRecording;
 
+    public void OnAfterDeserialize()
+    {
+        roundNum = 1;
+    }
 
+    public void OnBeforeSerialize()
+    {
+        
+    }
 }
